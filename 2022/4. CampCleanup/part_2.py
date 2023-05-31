e510378bdf0a3823f4fcb3389840f3d7b85a0c5f
Jonas Senft-Raiss <jonas.senftraiss@gmail.com>
2023-05-31 15:13:38 +0100

    solved part 2 of day 4 (and numbered files in order)

---
+++ 2022/4. CampCleanup/part_2.py
@@ -0,0 +1,22 @@
+# in format 'x-y' to return x, y
+def range_to_list(range):
+    [left, right] = range.split('-')
+    return [int(left), int(right)]
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+teams = [line.strip().split(',') for line in lines]
+
+count = 0
+
+for team in teams:
+    member_1 = range_to_list(team[0])
+    member_2 = range_to_list(team[1])
+    
+    if member_1[0] <= member_2[0] <= member_1[1]:
+        count += 1
+    elif member_2[0] <= member_1[0] <= member_2[1]:
+        count += 1
+    
+print(count)
